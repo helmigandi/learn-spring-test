be7ace912af1493ad0b00226471e0d161b47689a
helmigandi <sugandihelmi@gmail.com>
2024-04-17 08:13:10 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Learn Spring Test
+
+Software Testing with Spring Boot from Amigoscode.
+
+- Status: __On progress__
+- Progress video: Cloning Repo and Getting Started
+
+## Sources
+
+- [Software testing video course](https://app.amigoscode.com/p/software-testing)
+- [Software testing Github repository](https://github.com/amigoscode/software-testing)
---
+++ src/main/java/com/helmigandi/learnspringtest/LearnSpringTestApplication.java
@@ -0,0 +1,13 @@
+package com.helmigandi.learnspringtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnSpringTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnSpringTestApplication.class, args);
+	}
+
+}
